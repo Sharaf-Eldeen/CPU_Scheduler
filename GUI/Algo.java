703673aa78b855bc6e6f8a51fcee9843996c0b10
Mahmoud Elharty <138493314+MahmoudEhab3@users.noreply.github.com>
2024-12-06 22:21:08 +0200

    Update Algo.java

--- GUI/Algo.java
+++ GUI/Algo.java
@@ -1,48 +1,48 @@
-import java.util.List;
+// import java.util.List;
 
-public class Algo {
+// public class Algo {
 
-    public static Object[][] simulateSJF(List<Process> processes) {
-        Object[][] SJFoutput = {
-            {"P1", 0, 2, "#FF5733"},
-            {"Idle", 2, 3, "#D3D3D3"},
-            {"P2", 3, 5, "#33FF57"},
-            {"Idle", 5, 6, "#D3D3D3"},
-            {"P1", 6, 8, "#FF5733"}
-        };
-        return SJFoutput;
-    }
+//     public static Object[][] simulateSJF(List<Process> processes) {
+//         Object[][] SJFoutput = {
+//             {"P1", 0, 2, "#FF5733",5,5,5,5},
+//             {"Idle", 2, 3, "#D3D3D3,",5,5,5,5},
+//             {"P2", 3, 5, "#33FF57",5,5,5,5},
+//             {"Idle", 5, 6, "#D3D3D3",5,5,5,5},
+//             {"P1", 6, 8, "#FF5733",5,5,5,5}
+//         };
+//         return SJFoutput;
+//     }
 
-    public static Object[][] simulateSRTF(List<Process> processes) {
-        Object[][] SRTFoutput = {
-            {"P3", 0, 2, "#FF5733"},
-            {"Idle", 2, 3, "#D3D3D3"},
-            {"P2", 3, 5, "#33FF57"},
-            {"Idle", 5, 6, "#D3D3D3"},
-            {"P3", 6, 8, "#FF5733"}
-        };
-        return SRTFoutput;
-    }
+//     public static Object[][] simulateSRTF(List<Process> processes) {
+//         Object[][] SRTFoutput = {
+//             {"P1", 0, 2, "#FF5733",5,5,5,5},
+//             {"Idle", 2, 3, "#D3D3D3,",5,5,5,5},
+//             {"P2", 3, 5, "#33FF57",5,5,5,5},
+//             {"Idle", 5, 6, "#D3D3D3",5,5,5,5},
+//             {"P1", 6, 8, "#FF5733",5,5,5,5}
+//         };
+//         return SRTFoutput;
+//     }
 
-    public static Object[][] simulateFCAI(List<Process> processes) {
-        Object[][] FCAIoutput = {
-            {"P5", 0, 2, "#FF5733"},
-            {"Idle", 2, 3, "#D3D3D3"},
-            {"P2", 3, 5, "#33FF57"},
-            {"Idle", 5, 6, "#D3D3D3"},
-            {"P5", 6, 8, "#FF5733"}
-        };
-        return FCAIoutput;
-    }
+//     public static Object[][] simulateFCAI(List<Process> processes) {
+//         Object[][] FCAIoutput = {
+//             {"P1", 0, 2, "#FF5733",5,5,5,5},
+//             {"Idle", 2, 3, "#D3D3D3,",5,5,5,5},
+//             {"P2", 3, 5, "#33FF57",5,5,5,5},
+//             {"Idle", 5, 6, "#D3D3D3",5,5,5,5},
+//             {"P1", 6, 8, "#FF5733",5,5,5,5}
+//         };
+//         return FCAIoutput;
+//     }
 
-    public static Object[][] priorityScheduling(List<Process> processes) {
-        Object[][] PSoutput = {
-            {"P6", 0, 2, "#FF5733"},
-            {"Idle", 2, 3, "#D3D3D3"},
-            {"P2", 3, 5, "#33FF57"},
-            {"Idle", 5, 6, "#D3D3D3"},
-            {"P6", 6, 8, "#FF5733"}
-        };
-        return PSoutput;
-    }
-}
+//     public static Object[][] priorityScheduling(List<Process> processes) {
+//         Object[][] PSoutput = {
+//             {"P1", 0, 2, "#FF5733",5,5,5,5},
+//             {"Idle", 2, 3, "#D3D3D3,",5,5,5,5},
+//             {"P2", 3, 5, "#33FF57",5,5,5,5},
+//             {"Idle", 5, 6, "#D3D3D3",5,5,5,5},
+//             {"P1", 6, 8, "#FF5733",5,5,5,5}
+//         };
+//         return PSoutput;
+//     }
+// }
